a8e3837a1cfb61c6091cbaddaa42635ad01056ab
Mario Galindo <mgalindo9410@gmail.com>
2016-05-01 15:18:29 -0600

    Declaracion de arreglos

---
+++ video_18.rb
@@ -0,0 +1,29 @@
+#Arreglos
+
+=begin
+    
+=end
+
+
+class HolaMundo
+    def initialize()
+    end    
+    
+    def saluda()
+        
+        arreglo = [1,2,3]<<"Nuevo Valor"
+        arreglo.push("Otro")
+        puts arreglo[-1] 
+        
+         
+           
+    end
+       
+      
+               
+end
+
+
+
+objeto = HolaMundo.new()
+objeto.saluda
