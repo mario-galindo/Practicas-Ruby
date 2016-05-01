0dde637fb37d7d84d02caa8577f99214b67ce9db
Mario Galindo <mgalindo9410@gmail.com>
2016-05-01 16:52:03 -0600

    Metodo Constructor

--- clsPerro.rb
+++ clsPerro.rb
@@ -1,9 +1,14 @@
 class Perro
-    def initialize()
+    def initialize(nombre = "Sin Nombre")
+        @nombre = nombre
     end
     
     def Ladrar(nombre)
         puts "Gua Gua " + nombre
+    end
+    
+    def dame_nombre
+        return @nombre
     end
 
 end
---
+++ video_25.rb
@@ -0,0 +1,8 @@
+require_relative 'clsPerro.rb'
+
+obj =  Perro.new("Firulais")
+
+puts obj.dame_nombre
+
+obj.Ladrar("Mario")
+
